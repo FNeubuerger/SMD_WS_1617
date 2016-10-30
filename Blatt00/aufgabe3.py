cc3b55e8bd9026802e5c85cc81d29f5475e65a08
Felix Neubürger <felix.neubuerger@tu-dortmund.de>
2016-10-30 09:43:41 +0100

    aufgabe 1-3 ergebnisse fertig

--- Blatt00/aufgabe3.py
+++ Blatt00/aufgabe3.py
@@ -11,22 +11,48 @@ def aufg3():
 	###Definiere Funktionen###
 	##########################
 	def f(x):
-		return (np.sqrt(9-x)-3)/x
+		return (x**3 + 1/3) - (x**3 - 1/3)
 
+	def g(x):
+		return ((3 + (x**3)/3) - (3 - (x**3)/3))/x**3
 
-	xx = np.logspace(-20,-1,500)
-	def c(x): #grenzwert der Funktion
-		return -1/6 +0*x
+	xx = np.logspace(-20,20,41)
+
+	def f_alg(x): #grenzwert der Funktion
+		return 2/3 +0*x
+
+	print('für diese Werte weicht f(x) nur um 1% vom algebraischen Wert ab')
+	for i in xx:
+		if abs((f(i)-f_alg(i))/f_alg(i)) <=0.01:
+			print(i)
+	print('für diese Werte weicht g(x) nur um 1% vom algebraischen Wert ab')
+	for i in xx:
+		if abs((g(i)-f_alg(i))/f_alg(i)) <=0.01:
+			print(i)
+
+	print('für diese Werte ist f(x) = 0')
+	for i in xx:
+		if f(i)==0:
+			print(i)
+
+	print('für diese Werte ist g(x) = 0')
+	for i in xx:
+		if g(i)==0:
+			print(i)
+
 	##########################
 	#####Plotte Funktionen####
 	##########################
 
+
 	fig = plt.figure(figsize=(16,9))
-	plt.plot(xx,f(xx),'rx',label='f(x)')
-	plt.plot(xx,c(xx),'k--',label='analytischer Grenzwert')
+	plt.plot(xx,f(xx),'rx',markersize=5,label=r'$f(x)$')
+	plt.plot(xx,f_alg(xx),'k--',label=r'vereinfacht $f(x) = g(x) = 2/3$')
+	plt.plot(xx,g(xx),'bo',label=r'$g(x)$')
 	plt.xlabel(r'x')
 	plt.ylabel(r'Wert der Funktion')
 	plt.xscale('log')
+	#plt.yscale('log')
 	plt.legend(loc='best')
 	plt.savefig("plot3.png",dpi=300)
 	plt.show()
--- Blatt00/aufgabe4.py
+++ Blatt00/aufgabe4.py
@@ -1,1 +1,12 @@
- 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+plt.style.use('ggplot')
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.unicode'] = True
+plt.rcParams['font.family'] = 'lmodern'
+def aufg4():
+	
+
+if __name__ == '__main__':
+	aufg4()
