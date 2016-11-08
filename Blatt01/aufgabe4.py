48acc42523927200e80adf7e92c0eb942d73a7d3
helena nawrath <h.nawrath@yahoo.de>
2016-11-08 19:52:50 +0100

    fast abgabefertig

--- Blatt01/aufgabe4.py
+++ Blatt01/aufgabe4.py
@@ -60,12 +60,12 @@ def aufg4():
 	m_2=np.tan(2*np.pi*(np.rad2deg(alpha)+90)/360)
 	
 	#Plotten der Geraden u und v 
-	u = np.linspace(2.69,4)
-	plt.plot(u, 2-m_1*4+m_1*u, 'k.', label='Halbachsen')
-	v = np.linspace(3.75, 4) 
-	plt.plot(v, 2-m_2*4+m_2*v, 'k.')
-	print(p_1/2,p_2/2)
-	print(np.rad2deg(alpha))
+	#u = np.linspace(2.69,4)
+	#plt.plot(u, 2-m_1*4+m_1*u, 'k--', label='Halbachsen')
+	#v = np.linspace(3.75, 4) 
+	#plt.plot(v, 2-m_2*4+m_2*v, 'k--')
+	#print(p_1/2,p_2/2)
+	#print(np.rad2deg(alpha))
 	#Ellipsen plotten 
 	plt.axes()
 	ax = plt.gca()
@@ -78,17 +78,16 @@ def aufg4():
 	plt.gca().add_patch(ellipse)
 	plt.gca().add_patch(ellipse_2)
 	#Errorbars dazu
-	errX = sigma_1
-	errY = sigma_2
-	plt.errorbar(a_1 , a_2 , xerr=errY, yerr=errY, fmt='x',label="$\mu_x\pm\sigma_x,\mu_y\pm\sigma_y$")
+	#errX = sigma_1
+	#errY = sigma_2
+	#plt.errorbar(a_1 , a_2 , xerr=errY, yerr=errY, fmt='x',label="$\mu_x\pm\sigma_x,\mu_y\pm\sigma_y$")
 	plt.xlabel(r'x')
 	plt.ylabel(r'y')
 	plt.plot(0,0,'k-',label='$1\sigma$Ellipse')
 	plt.plot(0,0,'g-',label='Abfall auf 1/e$^{-1}$')
-	plt.colorbar(x,orientation='vertikal')
 	plt.legend(loc='best')
-
-	plt.savefig("plot_4e.png", dpi=150)
+	plt.colorbar().set_label('$p_1,p_2$' )
+	plt.savefig("plot_4b.png", dpi=150)
 	plt.show()
 
 
