bcf9719ab356e844c16d207c27f614506a4d2f4f
Felix Neubürger <felix.neubuerger@tu-dortmund.de>
2016-11-14 15:01:28 +0100

    plots zu 2 verändert

--- Blatt02/aufgabe3.py
+++ Blatt02/aufgabe3.py
@@ -7,48 +7,70 @@ plt.style.use('ggplot')
 plt.rcParams['text.usetex'] = True
 plt.rcParams['text.latex.unicode'] = True
 plt.rcParams['font.family'] = 'lmodern'
+
+
+from numpy.random import uniform
+def verwerfungsmethode(**kwargs):
+	while True:
+		size = kwargs.pop('n')
+		p = uniform(0, 1, size)
+		u = uniform(0, 1)
+		if u < kwargs.pop('func')(**kwargs):
+			return p
+
+def uniform_min_max(**kwargs):
+	return uniform(low=kwargs.pop('xmin'),high=kwargs.pop('xmax'),size=kwargs.pop('n'))
+
+def minmax(**kwargs):
+	xmin = kwargs.pop('xmin')
+	xmax = kwargs.pop('xmax')
+	x = np.linspace(xmin,xmax,(xmax-xmin+1))
+	return 1+0*x
+
+def exponential(**kwargs):
+	if min(t)<0:
+		print('Array t contains values smaller than zero. Must be larger or equal to zero')
+		return 0
+	else:
+		return kwargs.pop('N_pot')*np.exp(-kwargs.pop('t')/kwargs.pop('tau'))
+
+def pot(**kwargs):
+	xmin = kwargs.pop('xmin')
+	xmax = kwargs.pop('xmax')
+	exponent = kwargs.pop('exponent')
+	N = kwargs.pop('N_pot')
+	x = np.linspace(x_min,xmax,(xmax-xmin+1)*steps)
+	return N*x**(-exponent)
+
+def cauchy(**kwargs):
+	x = np.logspace(-20,20,kwargs.pop('n')+1)
+	return 1/np.pi * 1/(1+x**2)
+
+def a(xmin,xmax,size):
+	while True:
+		p = uniform(0, 1, size)
+		u = uniform(0, 1)
+		def minmax(p):
+			x = np.linspace(xmin,xmax,size)
+			return 1+0*x
+		if u < minmax(p):
+			return p
+
+def b(N,tau,size):
+	while True:
+		p = uniform(0, 1, size)
+		u = uniform(0, 1)
+		def minmax(p):
+			x = np.linspace(0,1e20,1e20+1)
+			return N * np.exp(-x/tau)
+		if u < exp(p):
+			return p
+
 def aufg3():
-	
-	x_min = 0
-	x_max = 1
-	s = np.random.uniform(low=x_min, high=x_max, size=100)
-	count_s, bins_s, ignored_s = plt.hist(s, 15, normed=True)
-	plt.plot(bins_s, np.ones_like(bins_s), linewidth=2, color='r')
-	plt.savefig('rnduniform.png',dpi=300)
-	#plt.show()
-	plt.close()
-
-	e = np.random.exponential(scale=1.0, size=1000)
-	count_e, bins_e, ignored_e = plt.hist(e, 15, normed=True,rasterized=True)
-	plt.savefig('rndexp.png',dpi=300)
-	#plt.show()	
-	plt.close()
-	
-	r = cauchy.rvs( size=1000)
-	count_r, bins_r, ignored_r = plt.hist(r, bins=np.linspace (-10. , 10., 50), normed=True,rasterized=True)
-	plt.savefig('rndcauchy.png',dpi=300)
-	#plt.show()
-	plt.close()
-	
-	data = np.load("empirisches_histogramm.npy")
-	plt.hist(data['bin_mid'], bins=np.linspace (0. , 1., 50) , weights =data['hist'],rasterized=True)
-	#plt.show()
-	plt.close()
-
-
-
-	xmin, xmax = min(data['bin_mid']), max(data['bin_mid'])  
-	lnspc = np.linspace(xmin, xmax, len(data['bin_mid']))
-	m = np.mean(data['bin_mid'])
-	s = np.std(data['bin_mid'])
-	print(m,s)
-	pdf_g = stats.norm.pdf(lnspc, m, s) # now get theoretical 	values in our interval  
-	
-	#plt.show()
-	plt.plot(lnspc, pdf_g) # plot it
-	#plt.show()
-	plt.close()
-	
+	xmin = 0
+	xmax = 5
+	arr_a = a(xmin=xmin,xmax=xmax,n=1000)
+	print(a)
 
 
 if __name__ == '__main__':
