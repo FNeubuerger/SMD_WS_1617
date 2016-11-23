22dd20af07d216821945f4a5970cc870b2e1ffb1
helena nawrath <h.nawrath@yahoo.de>
2016-11-23 10:18:33 +0100

    neuer Zettel

---
+++ Blatt04/aufgabe4.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+from matplotlib.patches import Ellipse
+import matplotlib.pyplot as plt
+import scipy.stats as scs
+def aufg4():
+
+
+
+
+
+if __name__ == '__main__':
+	aufg4()
+
+
+
