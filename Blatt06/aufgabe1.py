3e1d70ef11f0f3081d742a03a8a46135e5213f21
Felix Neubürger <felix.neubuerger@tu-dortmund.de>
2016-12-12 15:08:14 +0100

    aufgabe 1 fertig (noch texen)

--- Blatt06/aufgabe1.py
+++ Blatt06/aufgabe1.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import root_pandas as rpd
 import pandas as pd
 from ggplot import ggplot, aes, geom_point
-
+import time
+start_time = time.time()
 plt.style.use('ggplot')
 plt.rcParams['text.usetex'] = True
 plt.rcParams['text.latex.unicode'] = True
@@ -78,38 +79,113 @@ def plot(df, x, y, color):
     """
     print(ggplot(df, aes(x=x, y=y, color=color)) + geom_point())
 
+def performance(prediction,label):
+	#calculates precision, recall and accuracy of given prediction and label
+	prediction = np.array(prediction)
+	
+	label = np.array(label)
+	right = prediction[prediction==label]
+	wrong = prediction[prediction!=label]
+	sig = len(prediction[prediction==1]) #number of signal
+	
+	bkg = len(prediction[prediction==0])
+	#if bkg !=0:
+		#print(sig/bkg)
+	ratio=[]
+	significance=[]
+	if bkg!=0:
+		ratio.append((sig/bkg))
+	else:
+		ratio.append(0)
+	#print(ratio[0])
+	significance.append(sig/np.sqrt(sig+bkg))
+	#print(significance[0])
+	#number of tp fp tn and fn
+	tp = len(right[right==1])
+	fp = len(wrong[wrong==1])
+	tn = len(right[right==0])
+	fn = len(wrong[wrong==0])
+	
+	
+	if tp==0:
+		precision = 0
+		if fp==0: 
+			precision=1
+	else:
+		precision =  tp/(tp+fp)
+	if tp==0 and fn==0:
+		recall = 0
+	else:
+		recall = tp/(tp+fn)
+	if tp+tn==0:
+		accuracy=0
+	else:			
+		accuracy = (tp+tn)/(tp+tn+fn+fp)		
+	return precision,recall,accuracy,ratio[0],significance[0]
+
 def aufg1():
 	
 	#print(data)
 	#train_bg,test_bg=split_set(bg_l,ratio=0.5)
 	sig ,bg = read_data()
-	test,train = split_sets(sig,bg,100,100,100,200)
+	test,train = split_sets(sig,bg)
 	data=test.append(train)
 	test_labels=test['label']
 	train_labels=train['label']
 	#print(data)
 	#plot(data,'x','y',color='label')
-	result = test.apply(lambda row: knn(row,train,train_labels,k=20),axis=1)
+	#hier könnte man noch mal über k iterieren
+	result = test.apply(lambda row: knn(row,train,train_labels,k=10),axis=1)
 	#result.columns=['0','1']
 	#print(result)
 	
 
-	errors = result==test_labels 
+	#errors = result==test_labels 
 	
 	#print(errors) #tp wo true UND label=1...
-	print(errors.value_counts())
+	#print(errors.value_counts())
 
-	err_labels = pd.concat([errors,test_labels],axis=1)
-	err_labels.columns=['prediction','label']
-	print(err_labels)
+	#err_labels = pd.concat([result,test_labels],axis=1)
+	#err_labels.columns=['prediction','label']
+	#print(err_labels)
 	'''
 	if err_labels['prediction'][i]==True and err_labels['label'][i]==1
 		tp.append(1) #usw...
 	oder if errors[i]==True ...
 
 	oder bool durch int ersetzen oder andersrum und maske
-	'''
+	
+	tp = []
+	fp = []
+	tn = []
+	fn = []
+
+	for i in range(len(err_labels)):
+		if err_labels['prediction'][i]==True and err_labels['label'][i]==1 :
+			tp.append(1)
+
+		if err_labels['prediction'][i]==True and err_labels['label'][i]==0 :
+			fp.append(1)
+
+		if err_labels['prediction'][i]==False and err_labels['label'][i]==0 :
+			tn.append(1)
+
+		if err_labels['prediction'][i]==False and err_labels['label'][i]==1 :
+			fn.append(1)
 	
 
+
+	print('tp: ', tp)
+	print('fp: ', fp)
+	print('tn: ', tn)
+	print('fn: ', fn)
+	'''
+	prec, rec, acc, ratio, significance = performance(result,test_labels)
+	print('Reinheit ',prec)
+	print('Effizienz',rec)
+	print('Accuracy ',acc)
+	print('Signifikanz: ',significance)
+
+	print('---%s seconds---' %(time.time() -start_time))
 if __name__ == '__main__':
 	aufg1()
