28fa02e8b4cd26e9b1c5a26ac49def4c53e096c4
Felix Neubürger <felix.neubuerger@tu-dortmund.de>
2017-01-31 13:53:14 +0100

    aufgabe1 ipynb fertig

---
+++ Blatt11/aufgabe2.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+plt.style.use('ggplot')
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.unicode'] = True
+plt.rcParams['font.family'] = 'lmodern'
+def aufg2():
+
+
+
+	
+if __name__ == '__main__':
+	aufg2()
